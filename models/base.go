21f39b24b312c3807371d99496f941082d0f8cd0
Ângelo Costa Neto <angelo_costa_neto@outlook.com>
2021-09-16 11:54:23 -0300

    Initial Commit

---
+++ models/base.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+var db *gorm.DB
+
+func init() {
+	e := godotenv.Load() // Load .env file
+
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	fmt.Print(dbUri)
+
+	connection, error := gorm.Open("postgres", dbUri)
+
+	if error != nil {
+		fmt.Print(error)
+	}
+
+	db = connection
+	db.Debug().AutoMigrate(&Account{}, &Contact{}) //Database migration
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
